aa8d5292b9e3d031ce3da643008c610d1dd3615c
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-10-26 10:30:38 +0100

    Add initial route for services

---
+++ src/app/services/api/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+const apiAuth = require('login.dfe.api.auth');
+const config = require('./../../../infrastructure/config')();
+// const getUserAssociatedServices = require('./getUserAssociatedServices');
+
+const router = express.Router();
+
+const routeExport = () => {
+  // Add auth middleware.
+  router.use(apiAuth(router, config));
+
+  // Map routed to functions.
+  // router.get('/:userId', getUserAssociatedServices);
+
+  return router;
+};
+
+module.exports = routeExport();
