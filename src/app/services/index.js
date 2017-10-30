bc1f79b26dd88fe0a1d9fc2bb6b7a3087cf3e05b
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-10-30 15:38:59 +0000

    Implement API call for get Unassociated Users

--- src/app/services/index.js
+++ src/app/services/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const apiAuth = require('login.dfe.api.auth');
 const config = require('../../infrastructure/config/index')();
 const getUserAssociatedServices = require('./getUserAssociatedServices');
-const unassociatedWithUser = require('./unassociatedWithUser');
+const getUnassociatedWithUserServices = require('./getUnassociatedWithUserServices');
 
 const router = express.Router();
 
@@ -13,8 +13,8 @@ const routeExport = () => {
   router.use(apiAuth(router, config));
 
   // Map routed to functions.
-   router.get('/associated-with-user/:uid', getUserAssociatedServices);
-  router.get('/unassociatedWithUser/:userId', unassociatedWithUser);
+  router.get('/associated-with-user/:uid', getUserAssociatedServices);
+  router.get('/unassociated-with-user/:uid', getUnassociatedWithUserServices);
 
   return router;
 };
