dd3912f98b426ac39142d26412e9990b1ab2f21f
Simon Bull <simon.bull.84@googlemail.com>
2018-02-09 08:38:54 +0000

    Add create scripts for user_service_identifiers

---
+++ database_scripts/postgres/002_create_user_service_identifiers.sql
@@ -0,0 +1,10 @@
+create table if not exists services.user_service_identifiers
+(
+	user_id uuid not null,
+	service_id uuid not null,
+	organisation_id uuid not null,
+	identifier_key varchar(25) not null,
+	identifier_value varchar(255) not null,
+	constraint user_service_identifiers_pkey
+		primary key (user_id, service_id, organisation_id, identifier_key)
+);
