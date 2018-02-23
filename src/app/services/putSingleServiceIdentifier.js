77c88c59fdaeed4b6c2b30dd72a0cdf06ae452f7
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-02-23 21:18:19 +0000

    Add check to see if externalidentifier is unique

--- src/app/services/putSingleServiceIdentifier.js
+++ src/app/services/putSingleServiceIdentifier.js
@@ -1,4 +1,4 @@
-const { upsertExternalIdentifier } = require('./data/servicesStorage');
+const { upsertExternalIdentifier, getExternalIdentifier } = require('./data/servicesStorage');
 
 
 const putSingleServiceIdentifierForUser = async (req, res) => {
@@ -6,11 +6,17 @@ const putSingleServiceIdentifierForUser = async (req, res) => {
   const orgId = req.params.org_id ? req.params.org_id.toLowerCase() : '';
   const serviceId = req.params.sid ? req.params.sid.toLowerCase() : '';
   const userId = req.params.uid ? req.params.uid.toLowerCase() : '';
-  const identifierKey = req.body.idKey ? req.body.idKey.toLowerCase() : undefined;
-  const identifierValue = req.body.idValue ? req.body.idValue.toLowerCase() : '';
+  const identifierKey = req.body.id_key ? req.body.id_key.toLowerCase() : '';
+  const identifierValue = req.body.id_value ? req.body.id_value.toLowerCase() : '';
 
   if(!identifierKey) {
     return res.status(403).send();
+  }
+
+  const checkExternalIdentifier = await getExternalIdentifier(serviceId, identifierKey, identifierValue, req.header('x-correlation-id'));
+
+  if(checkExternalIdentifier) {
+    return res.status(409).send();
   }
 
   await upsertExternalIdentifier(serviceId, userId, orgId, identifierKey, identifierValue, req.header('x-correlation-id'));
