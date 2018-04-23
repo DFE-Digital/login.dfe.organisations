9937764703d816558b36c2110e8fe970ec914546
Simon Bull <simon.bull.84@googlemail.com>
2018-04-23 12:11:10 +0100

    Import groups and their associations

---
+++ tools/importGIASGroups.js
@@ -0,0 +1,7 @@
+const { importGroups } = require('./../src/app/giasImport');
+
+const doit = async () => {
+  await importGroups();
+};
+
+doit().then(() => console.info('done')).catch(e => console.error(e));
