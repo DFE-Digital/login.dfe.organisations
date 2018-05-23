623b4f06fd0048397f24a4b9bdcf98b3236a2abc
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-05-23 08:55:48 +0100

    Correct alter script

--- database_scripts/mssql/006_User_Organisation_update.sql
+++ database_scripts/mssql/006_User_Organisation_update.sql
@@ -1,6 +1,6 @@
 
 IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'user_organisation' AND COLUMN_NAME = 'status')
     BEGIN
-        ALTER TABLE user_organisation ADD COLUMN status smallint DEFAULT 0 NOT NULL
+        ALTER TABLE user_organisation ADD [status] smallint DEFAULT 0 NOT NULL
     END
 GO
