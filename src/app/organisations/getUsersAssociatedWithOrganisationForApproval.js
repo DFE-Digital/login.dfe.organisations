3923555f7687a634a9b63c0a50e32bb022a6704c
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-05-23 20:31:31 +0100

    Set uid to lowercase for consistency

--- src/app/organisations/getUsersAssociatedWithOrganisationForApproval.js
+++ src/app/organisations/getUsersAssociatedWithOrganisationForApproval.js
@@ -3,7 +3,9 @@ const { getUsersAssociatedWithOrganisationForApproval } = require('./data/organi
 
 const getUsersAssociatedWithOrganisation = async (req, res) => {
   try {
-    const userOrganisations = await getUsersAssociatedWithOrganisationForApproval(req.params.uid);
+    const userId = req.params.uid.toLowerCase();
+
+    const userOrganisations = await getUsersAssociatedWithOrganisationForApproval(userId);
     return res.contentType('json').send(userOrganisations);
   } catch (e) {
     logger.error(`Error getting user organisations for approval for user ${req.params.uid} - ${e.message}`);
