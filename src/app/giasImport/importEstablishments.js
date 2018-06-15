9f916d55155e52ca5750fb49904b54432ed643f7
Simon Bull <simon.bull.84@googlemail.com>
2018-06-15 07:42:07 +0100

    Include Higher education institutions in establishment import

--- src/app/giasImport/importEstablishments.js
+++ src/app/giasImport/importEstablishments.js
@@ -6,7 +6,7 @@ const uuid = require('uuid/v4');
 const uniqBy = require('lodash/uniqBy');
 
 const isEstablishmentImportable = (importing) => {
-  const importableTypes = ['01', '02', '03', '05', '06', '07', '08', '10', '11', '12', '14', '15', '18', '24', '25', '26', '28', '30', '32', '33', '34', '35', '36', '38', '39', '40', '41', '42', '43', '44', '45', '46'];
+  const importableTypes = ['01', '02', '03', '05', '06', '07', '08', '10', '11', '12', '14', '15', '18', '24', '25', '26', '28', '29', '30', '32', '33', '34', '35', '36', '38', '39', '40', '41', '42', '43', '44', '45', '46'];
   const importableStatuses = [1, 2, 3, 4];
 
   if (!importableTypes.find(t => t === importing.type)) {
