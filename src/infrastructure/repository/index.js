6ea9fe1093b778e42e63c087ca2cfcc96b33c993
Simon Bull <simon.bull.84@googlemail.com>
2018-11-02 09:23:44 +0000

    Add org type 003

--- src/infrastructure/repository/index.js
+++ src/infrastructure/repository/index.js
@@ -37,6 +37,7 @@ const defineStatic = (model) => {
   model.organisationCategory = [
     { id: '001', name: 'Establishment' },
     { id: '002', name: 'Local Authority' },
+    { id: '003', name: 'Other Legacy Organisations' },
     { id: '004', name: 'Early Year Setting' },
     { id: '008', name: 'Other Stakeholders' },
     { id: '009', name: 'Training Providers' },
