3a2a8da5567e44b37cfdbbea1df083e0bca417d6
Simon Bull <simon.bull.84@googlemail.com>
2018-12-16 15:31:07 +0000

    Remove org update notification until required

--- src/app/organisations/notifications/index.js
+++ src/app/organisations/notifications/index.js
@@ -1,12 +1,12 @@
-const config = require('./../../../infrastructure/config')();
-const { getOrgById } = require('./../data/organisationsStorage');
-const ServiceNotificationsClient = require('login.dfe.service-notifications.jobs.client');
+// const config = require('./../../../infrastructure/config')();
+// const { getOrgById } = require('./../data/organisationsStorage');
+// const ServiceNotificationsClient = require('login.dfe.service-notifications.jobs.client');
 
-const serviceNotificationsClient = new ServiceNotificationsClient(config.notifications.connectionString);
+// const serviceNotificationsClient = new ServiceNotificationsClient(config.notifications);
 
 const raiseNotificationThatOrganisationHasChanged = async (organisationId) => {
-  const organisation = await getOrgById(organisationId);
-  await serviceNotificationsClient.notifyOrganisationUpdated(organisation);
+  // const organisation = await getOrgById(organisationId);
+  // await serviceNotificationsClient.notifyOrganisationUpdated(organisation);
 };
 
 module.exports = {
