98841c147fa8b4d2e3f6d2eafd05882b1e458578
Simon Bull <simon.bull.84@googlemail.com>
2019-01-07 21:34:34 +0000

    Add user org text/numeric identifiers on invitation conversion

--- src/app/invitations/migrateInvitationToUser.js
+++ src/app/invitations/migrateInvitationToUser.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const config = require('./../../infrastructure/config')();
 const uuid = require('uuid/v4');
 const invitationStorage = require('./data/invitationsStorage');
 const serviceStorage = require('./../services/data/servicesStorage');
 const organisationsStorage = require('./../organisations/data/organisationsStorage');
+const { encodeNumberToString } = require('./../../utils');
 
 const APPROVED_STATUS = 1;
 
@@ -15,8 +17,29 @@ const handler = async (req, res) => {
   if (services) {
     for (let o = 0; o < services.length; o += 1) {
       const org = services[o];
+      let numericIdentifier;
+      let textIdentifier;
 
-      await organisationsStorage.setUserAccessToOrganisation(org.organisation.id, userId, org.role.id, APPROVED_STATUS, '');
+      if (config.toggles && config.toggles.generateUserOrgIdentifiers) {
+        numericIdentifier = await organisationsStorage.getNextUserOrgNumericIdentifier();
+
+        const options = encodeNumberToString(numericIdentifier);
+        let current;
+        let index = 1;
+        let inUse;
+        while ((!current || inUse) && index <= 5) {
+          current = options[`option${index}`];
+          const exiting = await organisationsStorage.getUserOrganisationByTextIdentifier(current);
+          inUse = exiting && !(exiting.user_id === userId && exiting.organisation_id === org.organisation.id);
+          index += 1;
+        }
+        if (inUse) {
+          return res.status(500).send();
+        }
+        textIdentifier = current;
+      }
+
+      await organisationsStorage.setUserAccessToOrganisation(org.organisation.id, userId, org.role.id, APPROVED_STATUS, '', numericIdentifier, textIdentifier);
 
       for (let s = 0; s < org.services.length; s += 1) {
         const svc = org.services[s];
