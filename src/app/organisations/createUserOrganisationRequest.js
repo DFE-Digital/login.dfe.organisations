db3d54471336b2160f3aa66ea0194442686dba94
Steve O'Hara <github@soharadesign.com>
2019-10-10 10:03:35 +0100

    [NSA-2248] Org request made to an org without approvers now correctly applies a 'No Approvers' status

--- src/app/organisations/createUserOrganisationRequest.js
+++ src/app/organisations/createUserOrganisationRequest.js
@@ -1,13 +1,19 @@
-const { createUserOrgRequest } = require('./data/organisationsStorage');
+const { createUserOrgRequest, getApproversForOrg } = require('./data/organisationsStorage');
 
 const createUserOrganisationRequest = async (req, res) => {
+  const approvers = [];
+
   if (!req.params.uid || !req.params.id) {
     return res.status(400).send();
   }
+
+  approvers = getApproversForOrg(req.params.id);
+
   const organisationRequest = {
     userId: req.params.uid,
     organisationId: req.params.id,
     reason: req.body.reason,
+    status: approvers != null && approvers.length == 0 ? 3 : 0,
   };
   const request = await createUserOrgRequest(organisationRequest);
   return res.status(201).send(request);
