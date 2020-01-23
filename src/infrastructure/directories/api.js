db8c40c20e44c87bc1afcc1bb2a8f059fab9df88
jovin-joy <jovin.joy@education.gov.uk>
2020-01-23 14:04:47 +0000

    NSA-2360

--- src/infrastructure/directories/api.js
+++ src/infrastructure/directories/api.js
@@ -34,6 +34,31 @@ const getUserById = async (uid, correlationId) => {
   }
 };
 
+const getUsersByIds = async (ids) => {
+  if (!ids) {
+    return undefined;
+  }
+  const token = await jwtStrategy(config.directories.service).getBearerToken();
+  try {
+    const users = await rp({
+      method: 'POST',
+      uri: `${config.directories.service.url}/users/by-ids`,
+      body: { ids },
+      headers: {
+        authorization: `bearer ${token}`,
+      },
+      json: true,
+    });
+    return users;
+  } catch (e) {
+    if (e.statusCode === 404) {
+      return undefined;
+    }
+    throw e;
+  }
+};
+
 module.exports = {
   getUserById,
+  getUsersByIds,
 };
