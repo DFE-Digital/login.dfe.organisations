f1b8ab438eec205b0a96ab28dc13b5fcf9a98082
Arshed <arshed@ability2code.co.uk>
2020-02-18 15:16:43 +0000

    Enhanced sql to make it easier to use

--- database_scripts/mssql/020_asp_service_role_restrictions.sql
+++ database_scripts/mssql/020_asp_service_role_restrictions.sql
@@ -1,8 +1,13 @@
-IF NOT EXISTS (SELECT serviceId FROM serviceParams WHERE serviceId='df2ae7f3-917a-4489-8a62-8b9b536a71cc' and paramName = 'maximumRolesAllowed')
+
+DECLARE @AspServiceId UNIQUEIDENTIFIER
+
+set @AspServiceId = (select top 1 serviceId from service where [name] = 'Analyse School Performance (ASP)')
+
+IF NOT EXISTS (SELECT serviceId FROM serviceParams WHERE serviceId=@AspServiceId and paramName = 'maximumRolesAllowed')
     BEGIN
       INSERT INTO serviceParams
       (serviceId, paramName, paramValue)
       VALUES
-      ('df2ae7f3-917a-4489-8a62-8b9b536a71cc', 'maximumRolesAllowed', 1)
+      ('df2ae7f3-917a-4489-8a62-8b9b536a71cc', 'maximumRolesAllowed', 1)    
     END
 GO
