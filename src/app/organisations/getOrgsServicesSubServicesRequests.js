3e0c24c196a6bc2979c673e2cb23299621b33129
Ionut Predoi <ionut.predoi@education.gov.uk>
2023-05-04 14:40:49 +0100

    NSA-6857 - Added unit tests

--- src/app/organisations/getOrgsServicesSubServicesRequests.js
+++ src/app/organisations/getOrgsServicesSubServicesRequests.js
@@ -6,7 +6,7 @@ const getOrgsServicesSubServicesRequests = async(req, res) => {
     const orgRequests = await getServiceAndSubServiceReqForOrgs(req.params.orgIds);
     return res.status(200).send(orgRequests);
   } catch (e) {
-    logger.error(`Error getting service and sub-service requests for organisation ${req.params.id} - ${e.message}`);
+    logger.error(`Error getting service and sub-service requests for organisation ${req.params.orgIds} - ${e.message}`);
     return res.status(500).send();
   }
 };
