b8cea7e1d602aa29dcad5ad26b8b9cb85b639707
jo-joy <jovin.joy@outlook.com>
2023-05-26 11:34:34 +0100

    Stop gias sync here as the new sync process is going live. keep the overdue requests alone here

--- src/worker.js
+++ src/worker.js
@@ -2,7 +2,6 @@ const logger = require('./infrastructure/logger');
 const config = require('./infrastructure/config')();
 const configSchema = require('./infrastructure/config/schema');
 const schedule = require('node-schedule');
-// const { importEstablishments, importAllGroupsData } = require('./app/giasImport');
 const overdueRequests = require('./app/overdueOrganisationRequests');
 const express = require('express');
 const healthCheck = require('login.dfe.healthcheck');
