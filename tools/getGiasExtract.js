3ba0e78d91b89ccda0e9e177211bdf82bb37a887
Lea Hayes <lea.hayes@education.gov.uk>
2024-04-25 10:35:43 +0100

    Remove unused files.
    Having collaborated with Jovin from the Pirean migration team it seems that these files are no longer needed.
    
    Removing these avoids the need to rework them to use the native `fetch` function (instead of `request-promise`).

--- tools/getGiasExtract.js
+++
@@ -1,7 +0,0 @@
-const { getExtract } = require('./../src/infrastructure/gias/webService');
-
-getExtract(13882)
-  .then(() => {
-    console.log('done');
-  })
-  .catch((e) => console.error(e.message));
