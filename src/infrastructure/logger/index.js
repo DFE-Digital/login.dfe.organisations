ea5293803f3613d893b485a292736cd95cbc224e
Ionut Predoi <ionut.predoi@education.gov.uk>
2024-11-08 12:58:03 +0000

    Update logger to hide audit logs

--- src/infrastructure/logger/index.js
+++ src/infrastructure/logger/index.js
@@ -1,51 +1,57 @@
-'use strict';
+const {
+  createLogger, format, transports, addColors
+} = require('winston');
 
-const winston = require('winston');
+const {
+  combine, prettyPrint, errors, simple, colorize
+} = format;
 const config = require('../config')();
 const appInsights = require('applicationinsights');
-const AuditTransporter = require('login.dfe.audit.transporter');
 const AppInsightsTransport = require('login.dfe.winston-appinsights');
 
 const logLevel = (config && config.loggerSettings && config.loggerSettings.logLevel) ? config.loggerSettings.logLevel : 'info';
 
-const customLevels = {
+const levelsAndColor = {
   levels: {
     audit: 0,
     error: 1,
     warn: 2,
     info: 3,
     verbose: 4,
-    debug: 5,
-    silly: 6
+    debug: 5
   },
   colors: {
-    info: 'yellow',
-    ok: 'green',
+    audit: 'magenta',
     error: 'red',
-    audit: 'magenta'
+    warn: 'yellow',
+    info: 'blue',
+    verbose: 'cyan',
+    debug: 'green'
   }
 };
 
+addColors(levelsAndColor.colors);
+// Formatter to hide audit records from other loggers.
+const hideAudit = format((info) => ((info.level.toLowerCase() === 'audit') ? false : info));
+
 const loggerConfig = {
-  levels: customLevels.levels,
+  levels: levelsAndColor.levels,
   transports: []
 };
 
-loggerConfig.transports.push(new (winston.transports.Console)({ level: logLevel, colorize: true }));
-
-const opts = { application: config.loggerSettings.applicationName, level: 'audit' };
-const auditTransport = AuditTransporter(opts);
-
-if (auditTransport) {
-  loggerConfig.transports.push(auditTransport);
-}
+loggerConfig.transports.push(new transports.Console({
+  format: combine(
+    hideAudit(),
+    colorize({ all: true }),
+    simple()
+  ),
+  level: logLevel
+}));
 
 if (config.hostingEnvironment.applicationInsights) {
-  appInsights.setup(config.hostingEnvironment.applicationInsights)
-    .setAutoCollectConsole(false, false)
-    .setSendLiveMetrics(config.loggerSettings.aiSendLiveMetrics || false)
-    .start();
+  appInsights.setup(config.hostingEnvironment.applicationInsights).setAutoCollectConsole(false, false).start();
   loggerConfig.transports.push(new AppInsightsTransport({
+    format: combine(hideAudit(), format.json()),
     client: appInsights.defaultClient,
     applicationName: config.loggerSettings.applicationName || 'Organisations',
     type: 'event',
@@ -53,10 +59,18 @@ if (config.hostingEnvironment.applicationInsights) {
   }));
 }
 
-const logger = winston.createLogger(loggerConfig);
+const logger = createLogger({
+  format: combine(
+    simple(),
+    errors({ stack: true }),
+    prettyPrint()
+  ),
+  transports: loggerConfig.transports,
+  levels: loggerConfig.levels
+});
 
 process.on('unhandledRejection', (reason, p) => {
-  logger.error('Unhandled Rejection at:', p, 'reason:', reason);
+  logger.error('Unhandled Rejection at:', p, 'reason: ', reason);
 });
 
 module.exports = logger;
