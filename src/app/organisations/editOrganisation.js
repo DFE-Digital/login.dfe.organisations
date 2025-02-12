d771282e1f2ba2b56ce4fc6a11aab0203246b7a4
Alex Platt <alex.platt@education.gov.uk>
2025-02-12 10:45:43 +0000

    removed comments
    removed comment
    
    removed comment

--- src/app/organisations/editOrganisation.js
+++ src/app/organisations/editOrganisation.js
@@ -1,24 +1,12 @@
-const {
-  //   add,
-  updateOtherStakeholders,
-  getOrgById,
-} = require("./data/organisationsStorage");
+const { updateOtherStakeholders } = require("./data/organisationsStorage");
 
 const editOrganisation = async (req, res) => {
-  console.log("editOrganisation called");
-  console.log("req: ", req.body);
-  console.log("params: ", req.params);
-
   const orgUpdate = {
     id: req.params.id,
     name: req.body.name,
     address: req.body.address,
   };
 
-  const orgId = req.params.id;
-  console.log("orgId: ", orgId);
-  const org = await getOrgById(orgId);
-  console.log("org: ", org);
   await updateOtherStakeholders(orgUpdate);
 
   return res.status(200).send();
