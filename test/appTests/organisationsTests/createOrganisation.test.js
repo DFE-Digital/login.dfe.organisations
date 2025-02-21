26cc952d4f5cefbb62fbdd03f556915aee1305bc
Alex Platt <alex.platt@education.gov.uk>
2025-02-21 17:46:16 +0000

    [NSA-8900] Improve test coverage in organisations

---
+++ test/appTests/organisationsTests/createOrganisation.test.js
@@ -0,0 +1,308 @@
+jest.mock(
+  "./../../../src/infrastructure/config",
+  () => () => require("../../utils").mockConfig(),
+);
+
+jest.mock("../../../src/app/organisations/data/organisationsStorage", () => ({
+  add: jest.fn(),
+  update: jest.fn(),
+  getOrgByUrn: jest.fn(),
+  getOrgByUid: jest.fn(),
+  getOrgByEstablishmentNumber: jest.fn(),
+  getOrgByUkprn: jest.fn(),
+  getOrgByLegacyId: jest.fn(),
+  getOrganisationCategories: jest.fn(),
+  getNextOrganisationLegacyId: jest.fn(),
+}));
+
+// jest.mock("./../../../src/app/organisations/data/organisationsStorage");
+jest.mock("./../../../src/app/organisations/notifications");
+// jest.mock("uuid");
+
+// const config = require("./../../../src/infrastructure/config")();
+const {
+  createOrganisation,
+  validateOrg,
+  getExistingOrg,
+  generateLegacyId,
+} = require("./../../../src/app/organisations/createOrganisation");
+const organisationStorage = require("./../../../src/app/organisations/data/organisationsStorage");
+const notifications = require("./../../../src/app/organisations/notifications");
+// const uuid = require("uuid");
+const httpMocks = require("node-mocks-http");
+
+const res = {
+  json: jest.fn(),
+  status: jest.fn(),
+  send: jest.fn(),
+  mockResetAll: function () {
+    this.json.mockReset().mockReturnValue(this);
+    this.status.mockReset().mockReturnValue(this);
+    this.send.mockReset().mockReturnValue(this);
+  },
+};
+
+describe("when creating a new organisation", () => {
+  let req;
+
+  beforeEach(() => {
+    req = httpMocks.createRequest({
+      body: {
+        name: "Test org 999",
+        address: "Test org 999 address",
+        ukprn: "",
+        category: { id: "008" },
+        upin: "",
+        urn: "",
+      },
+    });
+
+    res.mockResetAll();
+
+    organisationStorage.getOrganisationCategories.mockReset().mockReturnValue([
+      { id: "008", name: "Other Stakeholders" },
+      { id: "002", name: "Establishment" },
+    ]);
+
+    organisationStorage.getNextOrganisationLegacyId
+      .mockReset()
+      .mockReturnValue("738401");
+  });
+
+  test("should add a new organisation if none exists and generate a new legacyId", async () => {
+    await createOrganisation(req, res);
+    // const result = await generateLegacyId();
+
+    expect(organisationStorage.add).toHaveBeenCalled();
+    expect(generateLegacyId).toHaveBeenCalled();
+    expect(res.status).toHaveBeenCalledWith(201);
+    expect(
+      notifications.raiseNotificationThatOrganisationHasChanged,
+    ).toHaveBeenCalled();
+  });
+
+  test("should update an existing organisation if has a legacyId", async () => {
+    req = httpMocks.createRequest({
+      body: {
+        name: "Test org 999",
+        address: "Test org 999 address",
+        ukprn: "12345678",
+        category: { id: "008" },
+        upin: "",
+        urn: "",
+        legacyId: "1234",
+      },
+    });
+
+    organisationStorage.getOrgByLegacyId.mockReset().mockReturnValue({
+      name: "Test org 111",
+      address: "Test org 111 address",
+      ukprn: "12345678",
+      category: { id: "008" },
+      upin: "",
+      urn: "",
+      legacyId: "1234",
+    });
+
+    organisationStorage.getOrgByUrn.mockReset().mockReturnValue({
+      name: "Test org 111",
+      address: "Test org 111 address",
+      ukprn: "12345678",
+      category: { id: "008" },
+      upin: "",
+      urn: "4321",
+      legacyId: "1234",
+    });
+
+    await createOrganisation(req, res);
+
+    expect(organisationStorage.update).toHaveBeenCalledWith({
+      name: "Test org 999",
+      address: "Test org 111 address",
+      ukprn: "12345678",
+      category: { id: "008" },
+      upin: "",
+      urn: "",
+      legacyId: "1234",
+    });
+    expect(res.status).toHaveBeenCalledWith(202);
+    expect(
+      notifications.raiseNotificationThatOrganisationHasChanged,
+    ).toHaveBeenCalled();
+  });
+
+  test("should update an existing organisation if has a URN", async () => {
+    req = httpMocks.createRequest({
+      body: {
+        name: "Test org 999",
+        address: "Test org 111 address",
+        category: { id: "008" },
+        urn: "4321",
+      },
+    });
+
+    organisationStorage.getOrgByUrn.mockReset().mockReturnValue({
+      name: "Test org 111",
+      address: "Test org 111 address",
+      category: { id: "008" },
+      urn: "4321",
+    });
+
+    await createOrganisation(req, res);
+
+    expect(organisationStorage.update).toHaveBeenCalledWith({
+      name: "Test org 999",
+      address: "Test org 111 address",
+      category: { id: "008" },
+      urn: "4321",
+    });
+
+    expect(res.status).toHaveBeenCalledWith(202);
+    expect(
+      notifications.raiseNotificationThatOrganisationHasChanged,
+    ).toHaveBeenCalled();
+  });
+
+  test("should update an existing organisation if has a UID", async () => {
+    req = httpMocks.createRequest({
+      body: {
+        name: "Test org 999",
+        address: "Test org 111 address",
+        category: { id: "008" },
+        uid: "2025",
+      },
+    });
+
+    organisationStorage.getOrgByUid.mockReset().mockReturnValue({
+      name: "Test org 111",
+      address: "Test org 111 address",
+      category: { id: "008" },
+      uid: "2025",
+    });
+
+    await createOrganisation(req, res);
+
+    expect(organisationStorage.update).toHaveBeenCalledWith({
+      name: "Test org 999",
+      address: "Test org 111 address",
+      category: { id: "008" },
+      uid: "2025",
+    });
+
+    expect(res.status).toHaveBeenCalledWith(202);
+    expect(
+      notifications.raiseNotificationThatOrganisationHasChanged,
+    ).toHaveBeenCalled();
+  });
+
+  test("should update an existing organisation if it has a UKPRN", async () => {
+    req = httpMocks.createRequest({
+      body: {
+        name: "Test org 999",
+        address: "Test org 111 address",
+        category: { id: "008" },
+        ukprn: "12345678",
+      },
+    });
+
+    organisationStorage.getOrgByUkprn.mockReset().mockReturnValue({
+      name: "Test org 111",
+      address: "Test org 111 address",
+      category: { id: "008" },
+      ukprn: "12345678",
+    });
+
+    await createOrganisation(req, res);
+
+    expect(organisationStorage.update).toHaveBeenCalledWith({
+      name: "Test org 999",
+      address: "Test org 111 address",
+      category: { id: "008" },
+      ukprn: "12345678",
+    });
+
+    expect(res.status).toHaveBeenCalledWith(202);
+    expect(
+      notifications.raiseNotificationThatOrganisationHasChanged,
+    ).toHaveBeenCalled();
+  });
+
+  test("should return 400 if no organisation category is selected", async () => {
+    req = httpMocks.createRequest({
+      body: {
+        name: "Test org 999",
+        address: "Test org 999 address",
+        ukprn: "12345678",
+        category: { id: "" },
+        upin: "",
+        urn: "",
+        legacyId: "1234",
+      },
+    });
+
+    await createOrganisation(req, res);
+    const validateOrgResult = await validateOrg(req.body);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(validateOrgResult).toBe("Category is required");
+  });
+
+  test("should return 400 if incorrect organisation category is selected", async () => {
+    req = httpMocks.createRequest({
+      body: {
+        name: "Test org 999",
+        address: "Test org 999 address",
+        ukprn: "12345678",
+        category: { id: "999" },
+        upin: "",
+        urn: "",
+        legacyId: "1234",
+      },
+    });
+
+    await createOrganisation(req, res);
+    const validateOrgResult = await validateOrg(req.body);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(validateOrgResult).toBe("Unrecognised category 999");
+  });
+
+  test("should update establishment organisation if category 002 is selected and establishmentNumber provided", async () => {
+    req = httpMocks.createRequest({
+      body: {
+        name: "Test org 123",
+        address: "Test org 111 address",
+        category: { id: "002" },
+        establishmentNumber: "212121",
+      },
+    });
+
+    organisationStorage.getOrgByEstablishmentNumber
+      .mockReset()
+      .mockReturnValue({
+        name: "Test org 111",
+        address: "Test org 111 address",
+        category: { id: "002" },
+        establishmentNumber: "212121",
+      });
+    await createOrganisation(req, res);
+    const getExistingOrgResult = await getExistingOrg(req.body);
+
+    expect(organisationStorage.update).toHaveBeenCalledWith({
+      name: "Test org 123",
+      address: "Test org 111 address",
+      category: { id: "002" },
+      establishmentNumber: "212121",
+      legacyId: undefined,
+    });
+
+    expect(res.status).toHaveBeenCalledWith(202);
+    expect(getExistingOrgResult).toStrictEqual({
+      address: "Test org 111 address",
+      category: { id: "002" },
+      establishmentNumber: "212121",
+      legacyId: undefined,
+      name: "Test org 123",
+    });
+  });
+});
