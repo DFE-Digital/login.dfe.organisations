856a961ff69cfa817628d9a6014318ae7b1a1166
Alex Platt <alex.platt@education.gov.uk>
2025-02-25 16:56:28 +0000

    added test for add

---
+++ test/appTests/organisationsTests/add.test.js
@@ -0,0 +1,47 @@
+jest.mock(
+  "./../../../src/infrastructure/config",
+  () => () => require("../../utils").mockConfig(),
+);
+
+jest.mock("./../../../src/infrastructure/repository", () => {
+  //   const { createJestMockForSequelizeEntity } = require("./../../utils/mocks");
+
+  return {
+    organisations: {
+      create: jest.fn(),
+    },
+  };
+});
+
+const {
+  add,
+} = require("./../../../src/app/organisations/data/organisationsStorage");
+// const { createJestMockForSequelizeEntity } = require("./../../../utils/mocks");
+
+const { Op } = require("sequelize");
+const { organisations } = require("./../../../src/infrastructure/repository");
+
+describe("when calling add", () => {
+  let org;
+
+  beforeEach(() => {
+    org = {
+      id: "1234",
+      name: "Org-0",
+      address: "1 Abbey Road",
+      category: { id: "008" },
+      status: { id: "1" },
+    };
+
+    organisations.create.mockReset();
+  });
+
+  it("should add new org when existing record found", async () => {
+    const addResult = await add(org);
+
+    expect(organisations.create).toHaveBeenCalledWith(addResult);
+    expect(addResult.name).toBe("Org-0");
+    expect(addResult.Address).toBe("1 Abbey Road");
+    expect(addResult.Category).toBe("008");
+  });
+});
