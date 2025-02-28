e6986a307353af5e4ebea71b374fb229e57d7129
Alex Platt <alex.platt@education.gov.uk>
2025-02-28 13:14:23 +0000

    updated organisationStorage.add.test.js

--- test/appTests/organisationsTests/data/organisationStorage.add.test.js
+++ test/appTests/organisationsTests/data/organisationStorage.add.test.js
@@ -19,25 +19,62 @@ const { organisations } = require("../../../../src/infrastructure/repository");
 
 describe("when calling add", () => {
   let org;
+  let entity;
 
   beforeEach(() => {
     org = {
       id: "1234",
       name: "Org-0",
-      address: "1 Abbey Road",
+      Address: "1 Abbey Road",
       category: { id: "008" },
       status: { id: "1" },
+    };
+    entity = {
+      id: "1234",
+      name: "Org-0",
+      LegalName: undefined,
+      Category: "008",
+      Type: null,
+      URN: undefined,
+      UID: undefined,
+      UPIN: undefined,
+      UKPRN: undefined,
+      EstablishmentNumber: undefined,
+      Status: "1",
+      ClosedOn: undefined,
+      Address: undefined,
+      telephone: undefined,
+      regionCode: null,
+      phaseOfEducation: null,
+      statutoryLowAge: undefined,
+      statutoryHighAge: undefined,
+      legacyId: undefined,
+      companyRegistrationNumber: undefined,
+      SourceSystem: undefined,
+      ProviderTypeName: undefined,
+      ProviderTypeCode: undefined,
+      GIASProviderType: undefined,
+      PIMSProviderType: undefined,
+      PIMSProviderTypeCode: undefined,
+      PIMSStatusName: undefined,
+      PIMSStatus: undefined,
+      GIASStatusName: undefined,
+      GIASStatus: undefined,
+      MasterProviderStatusName: undefined,
+      MasterProviderStatusCode: undefined,
+      OpenedOn: undefined,
+      DistrictAdministrativeName: undefined,
+      DistrictAdministrativeCode: undefined,
+      DistrictAdministrative_code: undefined,
+      IsOnAPAR: undefined,
     };
 
     organisations.create.mockReset();
   });
 
   it("should add a new organisation", async () => {
-    const addResult = await add(org);
+    await add(org);
 
-    expect(organisations.create).toHaveBeenCalledWith(addResult);
-    expect(addResult.name).toBe("Org-0");
-    expect(addResult.Address).toBe("1 Abbey Road");
-    expect(addResult.Category).toBe("008");
+    expect(organisations.create).toHaveBeenCalledWith(entity);
   });
 });
